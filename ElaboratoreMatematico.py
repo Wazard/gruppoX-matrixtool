8b2dc25fc46e84c6b1456e67a5bef5cd7c7e9077
GiorgioDn <128035358+GiorgioDn@users.noreply.github.com>
2025-11-18 17:00:01 +0100

    Update ElaboratoreMatematico.py

--- ElaboratoreMatematico.py
+++ ElaboratoreMatematico.py
@@ -6,37 +6,25 @@ class ElaboratoreMatematico(MatrixHandler):
         super().__init__(data)
         
     def trasponi(self):
-        rows = len(matrix)
-        cols = len(matrix[0]) if rows > 0 else 0
+        rows = len(self.data)
+        cols = len(self.data[0]) if rows > 0 else 0
 
+        #crea una matrice di 0 di dimensione row x column
         tmp = [[0] * rows for _ in range(cols)]
 
+        #inverte gli elementi creando una nuova matrice
         for row in range(rows):
             for col in range(cols):
-                tmp[col][row] = matrix[row][col]
+                tmp[col][row] = self.data[row][col]
 
         return tmp
     
     def moltiplica_per_scalare(self, k:float):
-        result = []
-        for riga in self.data:
-            new_row = []
-            for elemento in riga:
-                new_row.append(elemento*k)
-            result.append(new_row)
+        rows = len(self.data)
+        cols = len(self.data[0]) if rows > 0 else 0
+        #moltiplico ogni elemento per k
+        for row in range(rows):
+            for col in range(cols):
+                self.data[row][col] *= k
                 
-        return result
-        
-#TEST
-matrix = [
-    [1, 2, 3],
-    [5, 6, 7],
-    [9, 10, 11]
-]
-
-elaboratore = ElaboratoreMatematico(matrix)
-
-trasponi = elaboratore.trasponi()
-matrix_trasponi = MatrixHandler(trasponi)
-
-matrix_trasponi.print_matrix()
+        return self.data
